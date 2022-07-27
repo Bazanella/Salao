f7bc2723b39dc89ef5117ae82fc810b34f6f622d
Marcelo Bazanella <marcelobaza1@hotmail.com>
2022-07-27 08:37:27 -0300

    Mensagem explicativa

--- Salao.dpr
+++ Salao.dpr
@@ -1,16 +1,24 @@
 program Salao;
 
 uses
-  Vcl.Forms,
-  uFrmPrincipal in 'uFrmPrincipal.pas' {FrmPrincipal},
-  uFrmPadrao in 'uFrmPadrao.pas' {FrmCadastroPadrao},
-  uFrmCadastroServicos in 'uFrmCadastroServicos.pas' {FrmCadastroPadrao1};
+  System.StartUpCopy,
+  FMX.Forms,
+  ufrmPrincipal in 'ufrmPrincipal.pas' {frmPrincipal},
+  uFrmCadastroPadrao in 'uFrmCadastroPadrao.pas' {frmCadastroPadrao},
+  uFrmCadastroServicos in 'uFrmCadastroServicos.pas' {frmCadastroServico},
+  uDmDados in 'uDmDados.pas' {dmDados: TDataModule},
+  uFrmCadastroClientes in 'uFrmCadastroClientes.pas' {frmClientes},
+  uFrmMensagensPadrao in 'uFrmMensagensPadrao.pas' {frmMensagemPadrao},
+  uFrmFaturamento in 'uFrmFaturamento.pas' {frmFaturamento},
+  uFrmCadastroProfissionais in 'uFrmCadastroProfissionais.pas' {frmCadastroProfissionais},
+  Classes.Utils.View in 'Classes\Classes.Utils.View.pas',
+  uFrmPagamentos in 'uFrmPagamentos.pas' {frmCadastroPagamentos};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TdmDados, dmDados);
   Application.Run;
 end.
